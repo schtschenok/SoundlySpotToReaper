ca04595a0ee045acf3417d3398581bf0390cdcf9
Тёма Макеев <tyomamakeev@gmail.com>
2020-01-21 15:38:21 +0300

    Much better way to run executable from Reaper

--- SoundlySpotToReaper/SoundlySpotToReaper.lua
+++ SoundlySpotToReaper/SoundlySpotToReaper.lua
@@ -2,25 +2,9 @@
 local ScriptPath = debug.getinfo(1,'S').source:match[[^@?(.*[\/])[^\/]-$]]
 --reaper.ShowConsoleMsg(ScriptPath)
 
--- Path to a .bat file to be created. We can't run an executable directly beacuse for some reason it stops executing when Reaper goes inactive, and this executable makes Reaper inactive, so we have to run it as a separate process. While this solution is not really beautiful, it's simple, and it works fast.
-local BatPath = ScriptPath .. "SoundlySpotToReaper.bat"
---reaper.ShowConsoleMsg(BatPath)
-
 -- Path to the executable
 local ExecutablePath = ScriptPath .. "SoundlySpotToReaper.exe"
 --reaper.ShowConsoleMsg(ExecutablePath)
 
--- Open the .bat file for writing
-local Bat = io.open(BatPath, "w")
-
--- Write code needed to execute SoundlySpotToReaper.exe to the .bat file
-Bat:write("start \"\" " .. ExecutablePath)
-
--- Close the .bat file
-Bat:close()
-
--- Run the .bat file
-os.execute(BatPath)
-
--- Delete the .bat file
-os.remove(BatPath)
+-- Run the executable
+os.execute("start " .. ExecutablePath)
